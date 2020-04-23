b36c7f2f21eec5088825ba9f6c0dc8f716f24ecb
Sungho Park <siminsungho@gmail.com>
2020-04-23 17:20:11 +0900

    Fix build error.

--- app/cli_tester_stm32footpad_seggerrtt.cmake
+++ app/cli_tester_stm32footpad_seggerrtt.cmake
@@ -3,11 +3,10 @@ set_cache(SEGGERRTT__DTTY_ENABLE TRUE BOOL)
 
 set_cache(UBINOS__UBICLIB__EXCLUDE_CLI FALSE BOOL)
 
-set_cache(UBINOS__BSP__STM32_RCC_HSE_CONFIG "ON" STRING)
-set_cache(UBINOS__BSP__STM32_HSE_VALUE "25000000U" STRING)
+set_cache(UBINOS__BSP__STM32F2_RCC_HSE_CONFIG "ON" STRING)
+set_cache(UBINOS__BSP__STM32F2_HSE_VALUE "25000000U" STRING)
 
 set_cache(UBINOS__BSP__OPENOCD_CONFIG_FILE "" PATH)
-
 set_cache(UBINOS__BSP__GDBSCRIPT_FILE_LOAD "${PROJECT_UBINOS_DIR}/resource/ubinos/bsp/arch/arm/cortexm/gdb_flash_load.gdb" PATH)
 set_cache(UBINOS__BSP__GDBSCRIPT_FILE_RESET "${PROJECT_UBINOS_DIR}/resource/ubinos/bsp/arch/arm/cortexm/gdb_flash_reset.gdb" PATH)
 
