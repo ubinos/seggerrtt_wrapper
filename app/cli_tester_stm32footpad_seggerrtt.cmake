e1aaa23d91b194e7686654649140e0f8c855cb8e
Sungho Park <siminsungho@gmail.com>
2022-01-21 00:09:08 +0900

    refactoring

--- app/cli_tester_stm32footpad_seggerrtt.cmake
+++ app/cli_tester_stm32footpad_seggerrtt.cmake
@@ -6,15 +6,18 @@
 
 # ubinos_config_info {"name_base": "cli_tester", "build_type": "cmake_ubinos", "app": true}
 
+set_cache(UBINOS__UBICLIB__EXCLUDE_CLI FALSE BOOL)
+
 set_cache(UBINOS__BSP__DTTY_TYPE "EXTERNAL" STRING)
 set_cache(SEGGERRTT__DTTY_ENABLE TRUE BOOL)
 
-set_cache(UBINOS__UBICLIB__EXCLUDE_CLI FALSE BOOL)
+set_cache(UBINOS__BSP__BOARD_VARIANT TRUE BOOL)
 
 set_cache(UBINOS__BSP__STM32_RCC_HSE_CONFIG "ON" STRING)
 set_cache(UBINOS__BSP__STM32_HSE_VALUE "25000000U" STRING)
 
-set_cache(UBINOS__BSP__OPENOCD_CONFIG_FILE "" PATH)
+set_cache(UBINOS__BSP__DEBUG_SERVER_TYPE "JLINK" STRING)
+set_cache(UBINOS__BSP__DEBUG_SERVER_COMMAND "JLinkGDBServer" STRING)
 set_cache(UBINOS__BSP__GDBSCRIPT_FILE_LOAD "${PROJECT_UBINOS_DIR}/resource/ubinos/bsp/arch/arm/cortexm/gdb_flash_load.gdb" PATH)
 set_cache(UBINOS__BSP__GDBSCRIPT_FILE_RESET "${PROJECT_UBINOS_DIR}/resource/ubinos/bsp/arch/arm/cortexm/gdb_flash_reset.gdb" PATH)
 
--- app/ubik_tester_stm32footpad_seggerrtt.cmake
+++ app/ubik_tester_stm32footpad_seggerrtt.cmake
@@ -9,10 +9,13 @@
 set_cache(UBINOS__BSP__DTTY_TYPE "EXTERNAL" STRING)
 set_cache(SEGGERRTT__DTTY_ENABLE TRUE BOOL)
 
+set_cache(UBINOS__BSP__BOARD_VARIANT TRUE BOOL)
+
 set_cache(UBINOS__BSP__STM32_RCC_HSE_CONFIG "ON" STRING)
 set_cache(UBINOS__BSP__STM32_HSE_VALUE "25000000U" STRING)
 
-set_cache(UBINOS__BSP__OPENOCD_CONFIG_FILE "" PATH)
+set_cache(UBINOS__BSP__DEBUG_SERVER_TYPE "JLINK" STRING)
+set_cache(UBINOS__BSP__DEBUG_SERVER_COMMAND "JLinkGDBServer" STRING)
 set_cache(UBINOS__BSP__GDBSCRIPT_FILE_LOAD "${PROJECT_UBINOS_DIR}/resource/ubinos/bsp/arch/arm/cortexm/gdb_flash_load.gdb" PATH)
 set_cache(UBINOS__BSP__GDBSCRIPT_FILE_RESET "${PROJECT_UBINOS_DIR}/resource/ubinos/bsp/arch/arm/cortexm/gdb_flash_reset.gdb" PATH)
 
