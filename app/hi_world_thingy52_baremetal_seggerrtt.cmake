c6fa0f9d4026bdf6a20641d55583dffab81ede1d
Sungho Park <siminsungho@gmail.com>
2023-09-27 05:46:11 +0900

    add app config, hi_world_thingy52_baremetal_seggerrtt, helloworld_world_thingy52_seggerrtt, ubik_tester_world_thingy52_seggerrtt, cli_world_thingy52_seggerrtt.

---
+++ app/hi_world_thingy52_baremetal_seggerrtt.cmake
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2021 Sung Ho Park and CSOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# ubinos_config_info {"name_base": "hi_world", "build_type": "cmake_ubinos", "app": true}
+
+set_cache(UBINOS__BSP__DTTY_TYPE "EXTERNAL" STRING)
+set_cache(SEGGERRTT__DTTY_ENABLE TRUE BOOL)
+
+include(${PROJECT_UBINOS_DIR}/config/ubinos_thingy52_baremetal.cmake)
+include(${PROJECT_LIBRARY_DIR}/seggerrtt_wrapper/config/seggerrtt.cmake)
+include(${PROJECT_LIBRARY_DIR}/nrf5sdk_extension/config/nrf5sdk_extension.cmake)
+
+####
+
+set(INCLUDE__APP TRUE)
+set(APP__NAME "hi_world")
+
+get_filename_component(_tmp_source_dir "${CMAKE_CURRENT_LIST_DIR}/${APP__NAME}" ABSOLUTE)
+
+file(GLOB_RECURSE _tmp_sources
+    "${_tmp_source_dir}/*.c"
+    "${_tmp_source_dir}/*.cpp"
+    "${_tmp_source_dir}/*.cc"
+    "${_tmp_source_dir}/*.S")
+
+set(PROJECT_APP_SOURCES ${PROJECT_APP_SOURCES} ${_tmp_sources})
+
